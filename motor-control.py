64c8285d7208aefa013ec176b8cb73f6f91c2f49
Mutjen <159343017+Mutjenn@users.noreply.github.com>
2024-02-09 23:10:22 +0300

    Update motor-control.py

--- motor-control.py
+++ motor-control.py
@@ -2,21 +2,21 @@ import time
 import Adafruit_PCA9685
 import RPi.GPIO as GPIO
 
-# GPIO pin definitions
+# GPIO pin belirleme
 GPIO.setmode(GPIO.BCM)
 up_down_motor_channels = [12, 13]  # üst alt motorlar
 forward_backward_motor_channels = [10, 11, 14, 15]  # ileri geri motorlar
 
-# ESC Calibration Values
+# ESC kalibrasyonu
 ESC_MIN = 205
 ESC_MAX = 405
 ESC_NEUTRAL = (ESC_MIN + ESC_MAX) // 2 #305 Orta değer
 
-# PCA9685 Initialization
+# PCA9685 kurulumu
 pca = Adafruit_PCA9685.PCA9685()
 pca.set_pwm_freq(50)  
 
-# Motor control functions
+# Motor kontrol fonksiyonları
 def set_motor_speed(channel, speed):
     pulse = int(ESC_NEUTRAL + speed * (ESC_MAX - ESC_NEUTRAL))
     pca.set_pwm(channel, 0, pulse)
@@ -32,34 +32,34 @@ try:
         user_input = input("Enter command: ").upper()
 
         if user_input == 'W':
-            # Move up gradually
+            
             for channel in up_down_motor_channels:
                 set_motor_speed(channel, 0.5) #yukarı
 
         elif user_input == 'S':
-            # Move down gradually
+            
             for channel in up_down_motor_channels:
                 set_motor_speed(channel, -0.5)#aşağı
 
         elif user_input == 'A':
-            # Go backward
+            
             for channel in forward_backward_motor_channels:
                 set_motor_speed(channel, -1.0)#geri
 
         elif user_input == 'D':
-            # Go forward
+            
             for channel in forward_backward_motor_channels:
                 set_motor_speed(channel, 1.0)#ileri
 
         elif user_input == 'L':
-            # Left turn
+            
             for channel in forward_backward_motor_channels[1:4]:  # Motors 2-4
                 set_motor_speed(channel, -1.0)
             for channel in forward_backward_motor_channels[2:3]:  # Motors 3-5
                 set_motor_speed(channel, 1.0)#sola dön
 
         elif user_input == 'R':
-            # Right turn
+            
             for channel in forward_backward_motor_channels[1:4]:  # Motors 2-4
                 set_motor_speed(channel, 1.0)
             for channel in forward_backward_motor_channels[2:3]:  # Motors 3-5
